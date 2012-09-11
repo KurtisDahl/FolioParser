e1b382e19d4a01c31899c4460760a38ae419b57c
KurtisDahl <kurtis@ratiointeractive.com>
2012-09-11 12:15:38 -0700

    assigned all the articles to vars in the default.js for debugging

--- FolioParser/js/default.js
+++ FolioParser/js/default.js
@@ -15,19 +15,19 @@
                 // TODO: This application has been newly launched. Initialize
                 // your application here.
                 var parser = new FolioParserComponent.Parser();
-                parser.parseIssue("IssueFolio.xml");
-                parser.parseArticle("10_1300_GQ0612_MRebel.Folio.xml");
-                parser.parseArticle("ArticleFolio.xml");
-                parser.parseArticle("00_0000_NavTo.Folio.xml");
-                parser.parseArticle("01_5150_LU0912_sspy04.Folio.xml");
-                parser.parseArticle("02_5800_AL0712_Profile_Folder.Folio.xml");
-                parser.parseArticle("03_4400_GQ0812_Norway.Folio.xml");
-                parser.parseArticle("04_2050_VO0311_Diary.Folio.xml");
-                parser.parseArticle("05_1500_SE0712_MOBstyTreySongz.Folio.xml");
-                parser.parseArticle("06_3000_TIES_BOWTIE44.Folio.xml");
-                parser.parseArticle("09_2100_VO0311_Photo1.Folio.xml");
-                
-                parser.parseArticle("11_2500_BA0812_Foodist.Folio.xml");
+                var issue = parser.parseIssue("IssueFolio.xml");
+                var mRebelArticle = parser.parseArticle("10_1300_GQ0612_MRebel.Folio.xml");
+                var articleFolioArticle = parser.parseArticle("ArticleFolio.xml");
+                var navToArticle = parser.parseArticle("00_0000_NavTo.Folio.xml");
+                var sspy04Article = parser.parseArticle("01_5150_LU0912_sspy04.Folio.xml");
+                var profileArticle = parser.parseArticle("02_5800_AL0712_Profile_Folder.Folio.xml");
+                var norwayArticle = parser.parseArticle("03_4400_GQ0812_Norway.Folio.xml");
+                var diaryArticle = parser.parseArticle("04_2050_VO0311_Diary.Folio.xml");
+                var mobstyTreySongzArticle = parser.parseArticle("05_1500_SE0712_MOBstyTreySongz.Folio.xml");
+                var bowTieArticle = parser.parseArticle("06_3000_TIES_BOWTIE44.Folio.xml");
+                var photo1Article = parser.parseArticle("09_2100_VO0311_Photo1.Folio.xml");
+                var foodistArticle = parser.parseArticle("11_2500_BA0812_Foodist.Folio.xml");
+
             } else {
                 // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
